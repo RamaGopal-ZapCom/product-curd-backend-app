834e4bd7f7a2c4ac4f5bc59e18c7d8765abe541a
RamaGopal-ZapCom <rama.gopal@zapcg.com>
2025-10-08 12:08:53 +0530

    added comments for Jasypt  encrypts and decrypts flow

--- src/main/java/com/product/config/JasyptConfig.java
+++ src/main/java/com/product/config/JasyptConfig.java
@@ -9,6 +9,45 @@ import org.springframework.context.annotation.Configuration;
 /**
  * Created by Rama Gopal
  * Project Name - product-curd-backend-app
+ *
+ *
+ *
+ *
+ * | Step | Description                                                            |
+ * | ---- | ---------------------------------------------------------------------- |
+ * | 1    | Add Jasypt dependency                                                  |
+ *
+ * <dependency>
+ *     <groupId>com.github.ulisesbocchio</groupId>
+ *     <artifactId>jasypt-spring-boot-starter</artifactId>
+ *     <version>3.0.5</version>
+ * </dependency>
+ *
+ * | 2    | Encrypt the sensitive property (DB password)                           |
+ * | 3    | Replace plain value with `ENC(...)` in `application.yml`               |
+ *
+ *      spring:
+ *   datasource:
+ *     url: jdbc:mysql://localhost:3306/full_stack_db
+ *     username: root
+ *     #password: rama123
+ *     password: ENC(RC8E36jlBmU5aH+jowlYfQ==)
+ *     driver-class-name: com.mysql.cj.jdbc.Driver
+ *
+ *
+ * | 4    | Run Spring Boot app with the key `                                      |
+ *
+ *      public class TestJspy {
+ *     public static void main(String[] args) {
+ *         BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+ *         textEncryptor.setPassword("secretkey");
+ *         String encrypted = textEncryptor.encrypt("rama123");
+ *         System.out.println("Encrypted password: " + encrypted);
+ *     }
+ * }
+ *
+ * | 5    | Jasypt decrypts automatically during startup                           |
+ *
  */
 @Configuration
 public class JasyptConfig {
